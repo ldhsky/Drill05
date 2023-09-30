95910fcbfdef34e7814f0c603ed43c1374863b70
ldhsky <ldhsky1027@gmail.com>
2023-09-30 21:15:06 +0900

    방향에 따른 이미지와 랜덤 위치에 손 이미지 생성 완료.
    캐릭터가 손가락을 향해 점점 느리게 이동하도록 하고, 손가락에 도착 시 is_arrive를 True로 바꿔 새로운 위치에 손가락 생성 시키는 코드 작성 필요

--- follow_hand_arrow.py
+++ follow_hand_arrow.py
@@ -1,3 +1,5 @@
+import random
+
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -7,6 +9,15 @@ character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
 moving = True
+
+is_arrive = True
+
+xPos, yPos = TUK_WIDTH // 2, TUK_HEIGHT // 2
+
+frame = 0
+
+prevframe = True
+
 def handle_events():
 
     global moving
@@ -18,15 +29,33 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             moving = False
 
-
+def draw_hand_arrow(hand):
+    hand_arrow.draw(hand[0], hand[1])
 
 while moving:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    # character.clip_draw()
+    if is_arrive:
+        HandPos = [random.randint(100, TUK_WIDTH - 100), random.randint(100, TUK_HEIGHT - 100)]
+        is_arrive = False
+    if HandPos[0] > xPos:
+        if prevframe:
+            prevframe = False
+            frame = 0
+        character.clip_draw(frame % 8 * 100, 100, 100, 100, xPos, yPos)
+
+    else:
+        if not prevframe:
+            prevframe = True
+            frame = 0
+        character.clip_draw(frame % 8 * 100, 0, 100, 100, xPos, yPos)
+
+    draw_hand_arrow(HandPos)
+
+    frame += 1
 
     update_canvas()
     handle_events()
-    delay(0.01)
+    delay(0.05)
 
 close_canvas()
